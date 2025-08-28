34bef916cd6ba895c896fd76288db339af324ac8
Gabriel Antonio <gabrantesrodrigues@gmail.com>
2025-08-28 20:26:38 -0300

    save

---
+++ 28-08/estrutra3.php
@@ -0,0 +1,20 @@
+<?php 
+    include("cabecalho.php");
+    $diaSemana = 3;
+
+    switch($diaSemana){
+        case 1:
+            echo "Hoje é Domingo!";
+            break;
+        case 2: 
+            echo "Hoje é Segunda";
+            break;
+        case 3: 
+            echo "Hoje é Terça";
+            break;
+        default:
+            echo"Hoje pode ser quarta, quinta sexta ou sabado";
+    }
+
+
+    include("rodape.php");
