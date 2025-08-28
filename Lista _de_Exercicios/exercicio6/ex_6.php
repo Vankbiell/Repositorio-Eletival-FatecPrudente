9ef73477d953b73d9e6bd275ed3abad5ea2b768c
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2025-08-28 07:25:36 -0300

    save

---
+++ Lista _de_Exercicios/exercicio6/ex_6.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercício 6 - Transformação de Celsius para Fahrenheit</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercício 6 - Transformação de Celsius para Fahrenheit</h1>
+<form method="post" action = "resex_6.php">
+<div class="mb-3">
+              <label for="celsius" class="form-label">Informe a temperatura em Celsius </label>
+              <input type="number" id="celsius" name="celsius" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
---
+++ Lista _de_Exercicios/exercicio7/resex_7.php
@@ -0,0 +1,26 @@
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Lista de Exercícios 1 - Exercício 4</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+    <div class = "conatainer mt-4">
+        <?php
+        $fahrenheit = $_POST["fahrenheit"];
+        $celsius = ($fahrenheit - 32) * 0.5556; // 5/9 ≈ 0.5556
+        echo "{$fahrenheit}°F = {$celsius}°C";
+        echo '<button type="button" onclick="history.back()" class="btn btn-primary">Voltar</button>'
+
+
+
+
+        //git config --global user.email "you@example.com"
+        //git config --global user.name ""
+        
+        ?>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</body>
+</html>
