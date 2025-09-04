eee5937939ea5a698d015a443bbf50aac30f4ea3
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2025-09-04 20:59:07 -0300

    Exercício 2 - Lista 2

--- Lista_de_Exercicios2/ex_2.php
+++ Lista_de_Exercicios2/ex_2.php
@@ -1,5 +1,15 @@
 <?php
 include("cabecalho.php");
-
+    $valor1 = 2;
+    $valor2 = 2;
+    if ($valor1 == $valor2){
+        $soma = $valor1 + $valor2;
+        $triplo = $soma * 3;
+        echo "Os dois valores são iguais então o tiplo da soma é: $triplo";
+    }
+    else{
+        $soma = $valor1 + $valor2;
+        echo"A soma dos dois números é: $soma";
+    }
 include ("rodape.php");
 ?>
