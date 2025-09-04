e097384f7f14c0471b4f4c5220e1f6f108c53f65
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2025-09-04 20:08:29 -0300

    save

--- 04-09/funcoes.php
+++ 04-09/funcoes.php
@@ -4,3 +4,30 @@
     echo "<p>Todas em maiúsculo:".strtolower($nome)."</p>";
     echo"<p>Quantidade de caracteres:".strlen($nome)."</p>";
     $posicao = strpos($nome,"e");
+    echo"<p>Caractere e na posição $posicao</p>";
+    date_default_timezone_set("America/Sao_Paulo");
+    $data1 = date("d/m/Y");
+    $dia = date("d");
+    $hora = date("H:i:s");
+    echo"<p>Data: $data1</p>";
+    echo"<p>Dia</p>";
+    echo"<p></p>";
+
+
+
+
+
+
+    $valor = -10;
+    echo"<p>Valor absoluto".abs($valor)."</p>";
+    $valor = 5.9;
+    echo"<p>Valor arredondado".round($valor)."</p>";
+    $valor = round(1,100);
+    echo"<p>Valor aléatorio: $valor</p>";
+    echo"<p>Raiz quadrada de 16:".sqtr(16)."</p>";
+    $valor = 13.5;
+    echo"<p>valor Formatado: R$".
+                number_format($valor,2,",",".")."</p>";
+
+
+    function
