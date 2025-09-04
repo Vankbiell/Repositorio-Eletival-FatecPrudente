59e1ddf625ca60399b7def38460e4fbc96cb7ead
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2025-09-04 20:48:38 -0300

    save

--- Lista_de_Exercicios2/ex_5.php
+++ Lista_de_Exercicios2/ex_5.php
@@ -1,5 +1,22 @@
 <?php
 include("cabecalho.php");
-
+    $numero = 2;
+        switch($numero){
+            case 1:
+                echo "<p>Janeiro</p>";
+                break;
+            case 2:
+                echo "<p>Fevereiro</p>";
+                break;
+            case 3:
+                echo "<p>Março</p>";
+                break;
+            case 4:
+                echo "<p>Abril</p>";
+                break;
+                //Adicionar outros meses
+            default:
+                 echo "<p>Número não possui mês correspondente!</p>";
+    }
 include ("rodape.php");
 ?>
--- Lista_de_Exercicios2/ex_9.php
+++ Lista_de_Exercicios2/ex_9.php
@@ -1,5 +1,11 @@
 <?php
 include("cabecalho.php");
-
+    $numero = 5;
+    $fatorial = $numero;
+    for($i=$numero-1;$i>1;$i--){
+        $fatorial = $fatorial * $i;
+        //$fatorial *= $i;
+    }
+    echo "O fatorial de $numero é: $fatorial";
 include ("rodape.php");
 ?>
