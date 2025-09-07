d43754faf32d9d59ac2c6e8304beac42f183981a
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2025-09-07 17:42:56 -0300

    Lista 2 - Exercicio 3

--- Lista_de_Exercicios2/ex_3.php
+++ Lista_de_Exercicios2/ex_3.php
@@ -1,5 +1,14 @@
 <?php
 include("cabecalho.php");
-    
+    $numero1 = 3;
+    $numero2 = 3;
+
+    if ($numero1 == $numero2)
+        echo "Os valores sâo iguais: $numero1";
+    elseif ($numero1 > $numero2)
+        echo " O valor A é o maior $numero1,$numero2";
+    else
+        echo " O valor B é o maior $numero2,$numero1";
+
 include ("rodape.php");
 ?>
