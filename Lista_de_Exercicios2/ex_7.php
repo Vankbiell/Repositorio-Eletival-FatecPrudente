d269a24e43d1080218b56c08a3a9b853cd65044a
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2025-09-07 18:45:52 -0300

    save

--- Lista_de_Exercicios2/ex_7.php
+++ Lista_de_Exercicios2/ex_7.php
@@ -1,5 +1,12 @@
 <?php
 include("cabecalho.php");
-
+    $valor = 10;
+    $total = 0;
+    $i = 1;
+    while($i <= $valor){
+        $total += $i;
+        $i++;
+    }
+    echo "<p>Total: $total</p>";
 include ("rodape.php");
 ?>
