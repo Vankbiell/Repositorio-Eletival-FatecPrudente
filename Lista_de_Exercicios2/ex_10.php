94cbf483b6df21781b1746d66594c5c7580aad88
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2025-09-07 19:08:27 -0300

    Lista 2 - Exercício 10(Encerrado)

--- Lista_de_Exercicios2/ex_10.php
+++ Lista_de_Exercicios2/ex_10.php
@@ -1,5 +1,10 @@
 <?php
 include("cabecalho.php");
-
+    $valor = 5;
+    echo"<h1>Tabuada</h1>";
+    for($i=1; $i<=10; $i++){
+        $resultado = $valor * $i;
+        echo"<p>$valor X $i = $resultado</p>";
+    }
 include ("rodape.php");
 ?>
