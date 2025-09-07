13393e1b6c19f77a16048c1c9ac6302d6bf9dbea
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2025-09-07 18:59:51 -0300

    Exercicício 8 - Lista 2

--- Lista_de_Exercicios2/ex_8.php
+++ Lista_de_Exercicios2/ex_8.php
@@ -1,5 +1,11 @@
 <?php
 include("cabecalho.php");
-    
+    $valor = 10;
+    $i = 1;
+    do{
+        echo"<p> Número $valor</p>";
+        $valor -= 1;
+        $i++;
+    }while($valor);
 include ("rodape.php");
 ?>
