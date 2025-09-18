c525c9df386cab03dc55e5c5b1948215ce8bb023
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2025-09-18 20:32:09 -0300

    save

--- Lista_de_Exercicios3/ex_10.php
+++ Lista_de_Exercicios3/ex_10.php
@@ -1,10 +1,24 @@
+
 <?php
 include("cabecalho.php");
-    $valor = 5;
-    echo"<h1>Tabuada</h1>";
-    for($i=1; $i<=10; $i++){
-        $resultado = $valor * $i;
-        echo"<p>$valor X $i = $resultado</p>";
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $valor = $_POST['valor'];
+        echo"<h1>Tabuada</h1>";
+        for($i=1; $i<=10; $i++){
+            $resultado = $valor * $i;
+            echo"<p>$valor X $i = $resultado</p>";
+        }
     }
-include ("rodape.php");
+?>
+
+<div class="container py-3">
+    <form method="post">
+        <div class="mb-3">
+            <label for="valor" class="form-label">Informe o 1º valor</label>
+            <input type="number" id="valor" name="valor" class="form-control" required="">
+        </div>
+    </form>
+</div>
+<?php
+    include ("rodape.php");
 ?>
