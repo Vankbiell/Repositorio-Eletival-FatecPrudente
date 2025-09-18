bc496ae62b8b19c5e8d5c3401553217fbd2c8f14
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2025-09-18 20:12:20 -0300

    save

--- 18-09/exercicio.php
+++ 18-09/exercicio.php
@@ -20,8 +20,14 @@
 </form>
 <?php 
   if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $vetor = $_POST['valor']
+    $vetor = $_POST['valor'];
+    sort($vetor);
+    rsort($vetor);
+    foreach($vetor as $v){
+      echo"<p>$v</p>";
+    }
   }
+  ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </div>
 </body>
