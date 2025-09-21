5cf24501f58ce568211ccf8dcf693c8faa1f7ae1
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2025-09-21 16:44:17 -0300

    save

--- Lista_de_Exercicios2/ex_6.php
+++ Lista_de_Exercicios2/ex_6.php
@@ -1,8 +1,22 @@
 
 <?php
 include("cabecalho.php");
-    $valor = 10;
+?>
+<div class="container py-3">
+    <form method="post">
+        <div class="mb-3">
+            <label for="valor" class="form-label">Informe o número para receber uma lista de 1 até seu número.</label>
+            <input type="number" id="valor" name="valor" class="form-control" required="">
+        </div>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+</div>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){ 
+    $valor = $_POST['valor'];
     for($i=1; $i<=$valor; $i++){
         echo"<p> Número $i</p>";
     }
+}
+include("rodape.php");
 ?>
