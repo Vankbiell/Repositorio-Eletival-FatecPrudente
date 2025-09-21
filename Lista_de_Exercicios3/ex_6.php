b7e932978936af2303fbd5369c06f7729ef90ea9
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2025-09-21 17:33:09 -0300

    save

--- Lista_de_Exercicios3/ex_6.php
+++ Lista_de_Exercicios3/ex_6.php
@@ -1,5 +1,17 @@
 <?php
 include("cabecalho.php");
+?>
+<div class="container py-3">
+    <form method="post">
+        <div class="mb-3">
+            <label for="numero" class="form-label">Informe o </label>
+            <input type="number" id="numero" name="numero" class="form-control" required="">
+        </div>
+    </form>
+</div>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
+}
 include ("rodape.php");
 ?>
