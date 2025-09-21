e4396a67362c9054121b3655343c00dc580c812c
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2025-09-21 16:22:14 -0300

    save

--- Lista_de_Exercicios2/ex_2.php
+++ Lista_de_Exercicios2/ex_2.php
@@ -1,7 +1,24 @@
 <?php
 include("cabecalho.php");
-    $valor1 = 2;
-    $valor2 = 2;
+?>
+<div class="container py-3">
+    <form method="post">
+        <div class="mb-3">
+            <label for="valor1" class="form-label">Informe o primeiro valor</label>
+            <input type="number" id="valor1" name="valor1" class="form-control" required="">
+        </div>
+        <div class="mb-3">
+            <label for="valor3" class="form-label">Informe o segundo valor</label>
+            <input type="number" id="valor2" name="valor2" class="form-control" required="">
+        </div>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+</div>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    
+    $valor1 = $_POST['valor1'];
+    $valor2 = $_POST['valor2'];
     if ($valor1 == $valor2){
         $soma = $valor1 + $valor2;
         $triplo = $soma * 3;
@@ -11,4 +28,5 @@ include("cabecalho.php");
         $soma = $valor1 + $valor2;
         echo"A soma dos dois números é: $soma";
     }
+}
 ?>
