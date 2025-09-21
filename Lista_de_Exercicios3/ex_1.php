dc2ab8894a8f3fe11d0351df7b2bdda5b3c6866a
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2025-09-21 19:38:23 -0300

    Exercicío 1 e 2 Lista 3

--- Lista_de_Exercicios3/ex_1.php
+++ Lista_de_Exercicios3/ex_1.php
@@ -4,14 +4,15 @@ include("cabecalho.php");
 <div class="container py-3">
     <form method="post">
         <div class="mb-3">
-            <label for="numero" class="form-label">Informe o </label>
-            <input type="number" id="numero" name="numero" class="form-control" required="">
+            <label for="palavra" class="form-label">Informe a palavra para receber a quantidade de caracteres </label>
+            <input type="text" id="palavara" name="palavra" class="form-control" required="">
         </div>
     </form>
 </div>
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-
+    $palavra = $_POST['palavra'];
+    echo "<p>Quantidade de caracteres da palavra:".strlen($palavra)."</p>";
 }
 include ("rodape.php");
 ?>
--- Lista_de_Exercicios3/ex_2.php
+++ Lista_de_Exercicios3/ex_2.php
@@ -4,14 +4,16 @@ include("cabecalho.php");
 <div class="container py-3">
     <form method="post">
         <div class="mb-3">
-            <label for="numero" class="form-label">Informe o </label>
-            <input type="number" id="numero" name="numero" class="form-control" required="">
+            <label for="palavra" class="form-label">Informe a palavra para receber todos a palavra com todos os caracteres em minúsculo e maiúsculo</label>
+            <input type="text" id="palavra" name="palavra" class="form-control" required="">
         </div>
     </form>
 </div>
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-
+    $palavra = $_POST['palavra'];
+    echo "<p>Todos os caracteres em maiúsculo: ".strtoupper($palavra)."</p>";
+    echo "<p>Todos os caracteres em minúsculo: ".strtolower($palavra)."</p>";
 }
 include ("rodape.php");
 ?>
