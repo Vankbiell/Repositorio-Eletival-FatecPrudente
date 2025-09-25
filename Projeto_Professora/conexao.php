e4b873bc63dba55796ac217352c3ab30b3ecde78
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2025-09-25 20:14:29 -0300

    save

--- Projeto_Professora/conexao.php
+++ Projeto_Professora/conexao.php
@@ -1,5 +1,12 @@
 <?php
 
-    $dominio = "mysql";//definição da string de conexão
-    
+    $dominio = "mysql:host=localhost;dbname:projetophp";//definição da string de conexão
+    $usuario = "root";
+    $senha = "";
+    try {
+        $pdo = new PDO($dominio, $usuario, $senha);
+    } catch(Exception $e) {
+        die();
+    }
+
 
