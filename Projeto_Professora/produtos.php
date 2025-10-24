ef5371f58f9032468cec77458935bba23b9d909c
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2025-10-23 22:07:56 -0300

    Aula23/10

--- Projeto_Professora/produtos.php
+++ Projeto_Professora/produtos.php
@@ -3,27 +3,32 @@
     require("conexao.php");
 
     try{
-        $stmt = $pdo->query("SELECT c. nome, p. * FROM produto p 
-                                INNER JOIN categoria c ON c.id = p.categoria_id");
+        $stmt = $pdo->query("SELECT c.nome, p.* FROM produto p
+                            INNER JOIN categoria c ON c.id = p.categoria_id");
         $dados = $stmt->fetchAll();
     } catch(\Exception $e){
         echo "Erro: ".$e->getMessage();
     }
 
     if (isset($_GET['cadastro']) && $_GET['cadastro']){
-        echo "<p class='text-success'>Cadastro de categoria realizado!</p>";
+        echo "<p class='text-success'>Cadastro realizado!</p>";
     } else if (isset($_GET['cadastro']) && !$_GET['cadastro']){
         echo "<p class='text-danger'>Erro ao cadastrar!</p>";
     }
-        if (isset($_GET['editar']) && $_GET['editar']){
-        echo "<p class='text-success'>Edição de categoria realizado!</p>";
+    if (isset($_GET['editar']) && $_GET['editar']){
+        echo "<p class='text-success'>Registro editado!</p>";
     } else if (isset($_GET['editar']) && !$_GET['editar']){
-        echo "<p class='text-danger'>Erro ao cadastrar!</p>";
+        echo "<p class='text-danger'>Erro ao editar!</p>";
+    }
+    if (isset($_GET['excluir']) && $_GET['excluir']){
+        echo "<p class='text-success'>Registro excluído!</p>";
+    } else if (isset($_GET['cadastro']) && !$_GET['cadastro']){
+        echo "<p class='text-danger'>Erro ao excluir!</p>";
     }
 ?>
 
 <h2>Produtos</h2>
-<a href="novo_produto.php" class="btn btn-success mb-3">Novo Registro de Produto</a>
+<a href="novo_produto.php" class="btn btn-success mb-3">Novo Registro</a>
 <table class="table table-hover table-striped">
     <thead>
         <tr>
@@ -41,7 +46,6 @@
             <td><?= $d['id'] ?></td>
             <td><?= $d['descricao'] ?></td>
             <td><?= $d['nome'] ?></td>
-            
             <td class="d-flex gap-2">
                 <a href="editar_produto.php?id=<?= $d['id'] ?>" class="btn btn-sm btn-warning">Editar</a>
                 <a href="consultar_produto.php?id=<?= $d['id'] ?>" class="btn btn-sm btn-info">Consultar</a>
