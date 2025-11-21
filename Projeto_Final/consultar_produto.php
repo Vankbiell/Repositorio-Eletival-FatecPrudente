167796099ecb8a6a60f6c4f728f0d01216937a5c
Gabriel Antônio <gabrantesrodrigues@gmail.com>
2025-11-21 20:27:58 -0300

    save

--- Projeto_Final/consultar_produto.php
+++ Projeto_Final/consultar_produto.php
@@ -3,22 +3,22 @@
     require("conexao.php");
     if($_SERVER['REQUEST_METHOD'] == "GET"){
         try{
-            $stmt = $pdo->prepare("SELECT * from categoria WHERE id = ?");
-            $stmt->execute([$_GET['id']]);
+            $stmt = $pdo->prepare("SELECT * from produto WHERE id_produto = ?");
+            $stmt->execute([$_GET['id_produto']]);
             $categoria = $stmt->fetch(PDO::FETCH_ASSOC);
         } catch (Exception $e){
-            echo "Erro ao consultar categoria: ".$e->getMessage();
+            echo "Erro ao consultar produto: ".$e->getMessage();
         }
     }
     if($_SERVER['REQUEST_METHOD'] == "POST"){
-        $id = $_POST['id'];
+        $id_produto = $_POST['id_produto'];
         try{
             $stmt = 
-                $pdo->prepare("DELETE from categoria WHERE id = ?");
-            if($stmt->execute([$id])){
-                header('location: categorias.php?excluir=true');
+                $pdo->prepare("DELETE from produto WHERE id_produto = ?");
+            if($stmt->execute([$id_produto])){
+                header('location: produto.php?excluir=true');
             } else {
-                header('location: categorias.php?excluir=false');
+                header('location: produto.php?excluir=false');
             }
         }catch(\Exception $e){
             echo "Erro: ".$e->getMessage();
@@ -26,12 +26,12 @@
     }
 ?>
 
-    <h1>Consultar Categoria</h1>
+    <h1>Consultar Produto</h1>
     <form method="post">
-        <input type="hidden" name="id" value="<?= $categoria['id'] ?>">
+        <input type="hidden" name="id_produto" value="<?= $pr['id_produto'] ?>">
         <div class="mb-3">
-            <label for="nome" class="form-label">Nome da categoria:</label>
-            <input disabled value="<?= $categoria['nome']?>" type="text" id="nome" name="nome" class="form-control" required="">
+            <label for="nome" class="form-label">Nome da produto:</label>
+            <input disabled value="<?= $pr['nome']?>" type="text" id_produto="nome" name="nome" class="form-control" required="">
         </div>
         <p>Deseja excluir esse registro?</p>
         <button type="submit" class="btn btn-danger">Excluir</button>
